0f39af8246540c9a71f959abff5ceb62245ba3ee
Mariell Hoversholm <proximyst@proximyst.com>
2021-09-08 14:59:03 +0200

    refactor: allow nullness ambiguity for local vars
    Signed-off-by: Mariell Hoversholm <proximyst@proximyst.com>

--- core/src/main/java/net/kyori/moonshine/package-info.java
+++ core/src/main/java/net/kyori/moonshine/package-info.java
@@ -15,8 +15,27 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-@DefaultQualifier(NonNull.class)
+@DefaultQualifier(
+    value = NonNull.class,
+    locations = {
+        TypeUseLocation.CONSTRUCTOR_RESULT,
+        TypeUseLocation.EXCEPTION_PARAMETER,
+        TypeUseLocation.EXPLICIT_LOWER_BOUND,
+        TypeUseLocation.EXPLICIT_UPPER_BOUND,
+        TypeUseLocation.FIELD,
+        TypeUseLocation.IMPLICIT_LOWER_BOUND,
+        TypeUseLocation.IMPLICIT_UPPER_BOUND,
+        TypeUseLocation.LOWER_BOUND,
+        TypeUseLocation.PARAMETER,
+        TypeUseLocation.RECEIVER,
+        TypeUseLocation.RESOURCE_VARIABLE,
+        TypeUseLocation.RETURN,
+        TypeUseLocation.UPPER_BOUND,
+        TypeUseLocation.OTHERWISE,
+    }
+)
 package net.kyori.moonshine;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
+import org.checkerframework.framework.qual.TypeUseLocation;
