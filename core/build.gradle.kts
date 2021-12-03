db3a90e669b690e213e3b253f015ad3401d9cb5c
Mariell Hoversholm <proximyst@proximy.st>
2021-12-03 13:42:22 +0100

    build: specify libs in libs.versions.toml
    Signed-off-by: Mariell Hoversholm <proximyst@proximy.st>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
 }
 
 dependencies {
-    api("io.leangen.geantyref:geantyref:1.3.11")
+    api(libs.geantyref)
     implementation(project(":moonshine-internal"))
     testImplementation(project(":moonshine-standard"))
-    testImplementation("net.kyori:examination-api:1.+")
-    testImplementation("net.kyori:examination-string:1.+")
+    testImplementation(libs.examination.api)
+    testImplementation(libs.examination.string)
 }
--- internal/build.gradle.kts
+++ internal/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    api("io.leangen.geantyref:geantyref:1.3.11")
+    api(libs.geantyref)
 }
